da69cfb22d3993ba8d8a7f4ced23e40ee5fe51a9
Szekely, Zolta <Zolta.Szekely@wolterskluwer.com>
2015-01-11 13:11:10 +0200

    Adding Action Buttons

--- app/src/main/java/com/example/zzolta/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/zzolta/myfirstapp/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -32,14 +32,25 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void openSettings() {
+
+    }
+
+    private void openSearch() {
+
     }
 
     public void sendMessage(View view) {
